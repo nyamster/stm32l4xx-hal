9733584b292a66cd57a905b339d0035839cd1d99
Roman M <m3angreen@gmail.com>
2021-06-30 09:48:12 +0300

    [stm32l4r9] Enable timers

--- src/lib.rs
+++ src/lib.rs
@@ -224,7 +224,8 @@ pub mod time;
     feature = "stm32l4x2",
     feature = "stm32l4x3",
     feature = "stm32l4x5",
-    feature = "stm32l4x6"
+    feature = "stm32l4x6",
+    feature = "stm32l4r9"
 ))]
 pub mod timer;
 #[cfg(any(
